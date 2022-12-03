79b2f4bc652b762dc4c36d8c7c135b591aa2e3c4
Mojtaba Ghasemi <35700482+mojtabaghaseemi@users.noreply.github.com>
2022-12-03 13:59:04 +0330

    Create config.py

---
+++ config.py
@@ -0,0 +1,17 @@
+# config
+backup_location = "/backup"
+backup_dirs = "/root/"
+mysql_backups = True
+clear_backups = True
+folder_docker_compose = "/root/"
+name_mysql_in_docker_compose = "mysql" # or [ mariadb or mysql-server ]
+
+mysql_pass = "TEST"
+mysql_username = "root"
+
+hosturl = "test.example.com"
+hostuser = "TEST"
+hostpass = "TEST"
+
+date_format = "%Y_%m_%d-%H_%M"
+backup_name_format = "%date%-%backupName%"
